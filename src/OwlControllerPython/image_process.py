7234aa4bd6ac5149c99b4cce9d3c4346c25d7816
LyokoJeremie <lucheng989898@gmail.com>
2023-02-20 09:20:40 +0800

    ..

--- src/OwlControllerPython/image_process.py
+++ src/OwlControllerPython/image_process.py
@@ -9,9 +9,9 @@ import io
 # https://stackoverflow.com/questions/26509715/how-to-download-image-to-memory-using-python-requests
 def parse_img(requestContent):
     """
-    从仿真平台中返回的无人机相机图像是一个标准html编码的png/jpg图像
+    从OwlAccessTerminal返回的无人机相机图像是一个标准html编码的png/jpg图像
     本函数使用opencv的cv::imdecode函数将其解析为cv::Mat图像数据
-    :param requestContent: 无人机相机图像数据 [TODO]
+    :param requestContent: 无人机相机图像数据
     :return: cv::Mat图像数据
     """
     im_arr = np.frombuffer(io.BytesIO(requestContent).getbuffer(), dtype=np.uint8)  # im_arr is one-dim Numpy array
